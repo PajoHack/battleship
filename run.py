03600069254f1da2a7837e90160550535d2523a6
Patrick Hackman <pajohack@gmail.com>
2023-02-27 13:15:37 +0000

    Add code to randomly generate the computers ships

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import random
 
 # Game setup
-game_area = 5
-user_board = [[' ' for _ in range(game_area)] for _ in range(game_area)]
-pc_board = [[' ' for _ in range(game_area)] for _ in range(game_area)]
+GAME_AREA = 5
+user_board = [[' ' for _ in range(GAME_AREA)] for _ in range(GAME_AREA)]
+pc_board = [[' ' for _ in range(GAME_AREA)] for _ in range(GAME_AREA)]
 user_name = input("Please enter your name: ")
 
-# User places ships on the board
+# Welcome message and user asked to place ships on the board
+# Invalid enteries checked and user is asked to re-enter input if location
+# outside of the board is selected or if anything other than
+# an int between 0 - 4 is inputted. User is also informed if they have already selected
+# this location on the board.
 print((f"{user_name}, Welcome to Battleship. Place your ships on the board to start!"))
 for i in range(3):
     permitted_location = False
@@ -15,7 +19,7 @@ for i in range(3):
             row = int(input(f"Please select row for ship {i+1}: "))
             col = int(input(f"Please select column for ship {i+1} "))
             print("\n")
-            if row < game_area and col < game_area:
+            if row < GAME_AREA and col < GAME_AREA:
                 if user_board[row][col] == ' ':
                     user_board[row][col] == 'S'
                     permitted_location = True
@@ -26,5 +30,22 @@ for i in range(3):
         except ValueError:
             print("Input is not valid. Please enter a whole number.")
 
-# PC's ships are created
+# PC's ships are created and placed in random locations on the board. 
+print("The computer is randomly selecting locations for their ships.")
+for i in range(3):
+    permitted_location = False
+    while not permitted_location:
+        row = random.randint(0, GAME_AREA-1)
+        col = random.randint(0, GAME_AREA-1)
+        if pc_board[row][col] == ' ':
+            pc_board[row][col] == 'S'
+            permitted_location = True
 
+
+# Function to print the board to the terminal
+def display_board(area):
+    print("   " + " ".join(str(i) for i in range(GAME_AREA)))
+    print("  +" + "--" * GAME_AREA + "+")
+    for i in range(board_size):
+        print(f"{i} | {' '.join(area[i])} |")
+    print("  +" + "--" * GAME_AREA + "+")
