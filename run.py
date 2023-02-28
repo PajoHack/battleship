9e21f5ee25570ac5270e3eb263f5c98da66b04a4
Patrick Hackman <pajohack@gmail.com>
2023-02-28 21:08:08 +0000

    Create game_setup function and moved the setup code into it

--- run.py
+++ run.py
@@ -19,43 +19,52 @@ col = column
 """
 print(LEGEND)
 
-# Welcome message and user asked to place ships on the board
-# Invalid enteries checked and user is asked to re-enter input if location
-# outside of the board is selected or if anything other than
-# an int between 0 - 4 is inputted. User is also informed if they have already 
-# selected this location on the board.
-print((f"{user_name}, Welcome to Battleship!"))
-print("Begin by placing 3 battleships on the board.")
-print("The board is 5 x 5. please enter 0 - 4 for your selections.\n")
-for i in range(3):
-    permitted_location = False
-    while not permitted_location:
-        try:
-            row = int(input(f"Please select row for ship {i + 1}: "))
-            col = int(input(f"Please select col for ship {i + 1}: "))
-            print("\n")
-            if row < GAME_AREA and col < GAME_AREA:
-                if user_board[row][col] == ' ':
-                    user_board[row][col] = 'S'
-                    permitted_location = True
+def game_setup(GAME_AREA, user_board, pc_board, user_name):
+    """
+    function asks user to place 3 ships on the board.
+    The function also randomly places 3 ships on the board for the computer.
+    The function checks if the user has entered a valid location for
+    their ships. If the user has entered an invalid location they are
+    asked to re-enter the location. The function also checks if the
+    computer has randomly placed a ship in a location
+    that already has a ship. If the computer has placed a ship in a
+    location that already has a ship,
+    the function will randomly select a new location for the ship.
+    """
+    print((f"{user_name}, Welcome to Battleship!"))
+    print("Begin by placing 3 battleships on the board.")
+    print("The board is 5 x 5. please enter 0 - 4 for your selections.\n")
+    for i in range(3):
+        permitted_location = False
+        while not permitted_location:
+            try:
+                row = int(input(f"Please select row for ship {i + 1}: "))
+                col = int(input(f"Please select col for ship {i + 1}: "))
+                print("\n")
+                if row < GAME_AREA and col < GAME_AREA:
+                    if user_board[row][col] == ' ':
+                        user_board[row][col] = 'S'
+                        permitted_location = True
+                    else:
+                        print("A ship is already in this location.")
                 else:
-                    print("A ship is already in this location.")
-            else:
-                print(f"Location is not on the board. Choose between 0 and 4.")
-        except ValueError:
-            print("Input is not valid. Please enter a whole number.")
+                    print(f"Location is not on the board. Choose between 0 and 4.")
+            except ValueError:
+                print("Input is not valid. Please enter a whole number.")
 
 
-# PC's ships are created and placed in random locations on the board. 
-print("The computer is randomly selecting locations for their ships.\n")
-for i in range(3):
-    permitted_location = False
-    while not permitted_location:
-        row = random.randint(0, GAME_AREA-1)
-        col = random.randint(0, GAME_AREA-1)
-        if pc_board[row][col] == ' ':
-            pc_board[row][col] = 'S'
-            permitted_location = True
+    # PC's ships are created and placed in random locations on the board. 
+    print("The computer is randomly selecting locations for their ships.\n")
+    for i in range(3):
+        permitted_location = False
+        while not permitted_location:
+            row = random.randint(0, GAME_AREA-1)
+            col = random.randint(0, GAME_AREA-1)
+            if pc_board[row][col] == ' ':
+                pc_board[row][col] = 'S'
+                permitted_location = True
+
+game_setup(GAME_AREA, user_board, pc_board, user_name)
 
 
 def display_board(area):
